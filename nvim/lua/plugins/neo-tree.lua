ffdec669d36853a2b2021e9ae48f909604855dc5
Modgamedev <modgamedev@gmail.com>
2025-11-09 18:48:31 +0500

    Update neo-tree.lua

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,45 +1,34 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",          -- сам плагин Neo-tree
-  branch = "v3.x",                        -- ветка плагина
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
-    "nvim-lua/plenary.nvim",              -- зависимость: утилиты Lua
-    "MunifTanjim/nui.nvim",               -- зависимость: UI-компоненты
-    "nvim-tree/nvim-web-devicons",        -- зависимость: иконки файлов
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
   },
   -- Конфигурация neo-tree
   opts = {
-    close_if_last_window = true,             -- закрывать Neo-tree, если больше нет окон
-    enable_git_status = true,                -- показывать git-статус
-    open_files_using_relative_paths = true,  -- открывать файлы с относительными путями
+    close_if_last_window = true, -- закрывать Neo-tree, если больше нет окон
+    enable_git_status = true,    -- показывать git-статус
     -- Настройки окна neo-tree
     window = {
-      position = "left",                     -- позиция окна (слева)
-      width = 30,                            -- ширина окна
+      position = "left",
+      width = 30,
     },
     -- Настройки внешнего вида компонентов
     default_component_configs = {
       container = {
-        enable_character_fade = true,        -- края названия длинных файлов "затухают"
+        enable_character_fade = true, -- края длинных файлов "затухают"
+      },
+      file_size = {
+        enabled = true,          -- показывать колонку с размером файла
+        width = 12,              -- ширина колонки в символах
+        required_width = 64,     -- минимальная ширина окна, чтобы колонка отображалась
       },
     },
     -- Настройки буфера обмена
     clipboard = {
-      sync = "universal",                    -- копирование доступно во всех Neovim и в системе
-    },
-    -- Настройки отступов и стрелок в дереве
-    indent = {
-      indent_size = 2,                       -- размер одного уровня вложенности в пробелах
-      padding = 1,                           -- дополнительный отступ слева
-      -- линии отступов
-      with_markers = true,                   -- включить линии отступов (guides)
-      indent_marker = "│",                   -- символ для вертикальных линий
-      last_indent_marker = "└",              -- символ для последнего элемента ветки
-      highlight = "NeoTreeIndentMarker",     -- группа подсветки для линий отступов
-      -- стрелки для вложенных файлов/папок
-      with_expanders = nil,                   -- nil = включить автоматические стрелки, если включено вложение файлов
-      expander_collapsed = "",               -- символ для свернутой папки
-      expander_expanded = "",                -- символ для раскрытой папки
-      expander_highlight = "NeoTreeExpander", -- группа подсветки для стрелок
+      sync = "universal", -- копирование доступно во всех Neovim и в системе
     },
   },
 }
