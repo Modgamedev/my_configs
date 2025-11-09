c3815d9fb422213b55cb68ded95d68c808c3b190
Modgamedev <modgamedev@gmail.com>
2025-11-09 15:13:05 +0500

    Update tokyonight.lua

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -3,10 +3,8 @@ return {
     "folke/tokyonight.nvim",  -- название темы
     lazy = false,             -- загружается сразу при старте
     priority = 1000,          -- загружается раньше других плагинов
-    opts = {},                -- здесь можно добавить настройки темы, пока пусто
-    config = function(_, opts)
-      require("tokyonight").setup(opts)  -- применяем настройки
-      vim.cmd([[colorscheme tokyonight]]) -- включаем тему
+    config = function()       -- Настройки плагина ниже
+      vim.cmd([[colorscheme tokyonight-night]])
     end,
   }
 }
